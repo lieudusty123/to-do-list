0792e697c81dbc7d7178470014c1bc9e694e00cd
Omer Ezra <90283262+lieudusty123@users.noreply.github.com>
2022-08-07 23:39:39 +0300

    fixed design bugs, removed debug tasks

--- src/scripts/Tasks.js
+++ src/scripts/Tasks.js
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import TasksLists from "./TasksLists";
-import Info from "./Info";
 
 export default function Tasks(props) {
     const [textInput, setTextInput] = useState('')
